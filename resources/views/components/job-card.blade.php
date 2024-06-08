34738d02e1b8e29e833d4e02b5d8828a6e402483
estevan-r <120763362+estevan-r@users.noreply.github.com>
2024-06-08 17:12:05 -0600

    Initial set up with starter layout and components

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,18 @@
+<div class="flex flex-col rounded-xl bg-white/5 p-4 text-center">
+    <div class="self-start text-sm">Laracasts</div>
+
+    <div class="py-8 font-bold">
+        <h3>Video Producer</h3>
+        <p>Full Time - from $60,000</p>
+    </div>
+
+    <div class="mt-auto flex items-center justify-between">
+        <div class="space-x-1">
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+        <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+    </div>
+</div>
---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,3 @@
+<a href="#" class="rounded-xl bg-white/10 px-2 py-1 text-xs transition-colors duration-300 hover:bg-white/25">
+    {{ $slot }}
+</a>
